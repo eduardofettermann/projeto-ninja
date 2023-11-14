e3e4d77efd888dac67c35e97db7210a95faaca25
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-14 20:12:03 -0300

    first commit

---
+++ src/main/java/com/github/projetoninja/ProjetoNinjaApplication.java
@@ -0,0 +1,13 @@
+package com.github.projetoninja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoNinjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/projetoninja/ProjetoNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.projetoninja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
