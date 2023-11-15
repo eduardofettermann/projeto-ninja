af8f09abba7234fb5f167dee7e65deff4a5b5804
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-15 16:10:17 -0300

    refactor(endpoint/MissaoController): troca do RequestMapping: missoes -> projetoninja, e os demais companhados de / e o metodo

--- src/main/java/com/github/projetoninja/controller/MissaoController.java
+++ src/main/java/com/github/projetoninja/controller/MissaoController.java
@@ -9,27 +9,27 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/missoes")
+@RequestMapping("/projetoninja")
 public class MissaoController {
     @Autowired
     private MissaoService missaoService;
 
-    @PostMapping("/cadastrar")
+    @PostMapping("/missoes/cadastrar")
     public Missao cadastrarMissao(@RequestBody Missao missao) {
         return missaoService.cadastrarMissao(missao);
     }
 
-    @GetMapping("/id={id}")
+    @GetMapping("/missoes/{id}")
     public Optional<Missao> retornarMissaoPorId(@PathVariable Long id){
         return missaoService.retornarMissaoPeloId(id);
     }
 
-    @GetMapping
+    @GetMapping("/missoes")
     public List<Missao> retornarMissoes(){
         return missaoService.retornarTodasMissoes();
     }
 
-    @PutMapping("/alterar")
+    @PutMapping("/missoes/alterar")
     public Missao alterarMissao(@RequestParam(value = "id") Long id ,@RequestBody Missao missaoAlterada){
         return missaoService.alterarMissao(id,missaoAlterada);
     }
