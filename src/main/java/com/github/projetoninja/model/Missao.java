3b68503a9477d6f1807ae5a33285b3a4995c9bf6
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-14 21:32:50 -0300

    feat(model): adicionando o corpo esquecido da classe Missao

--- src/main/java/com/github/projetoninja/model/Missao.java
+++ src/main/java/com/github/projetoninja/model/Missao.java
@@ -1,2 +1,27 @@
-package com.github.projetoninja.model;public class Missao {
+package com.github.projetoninja.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@Table(name = "missoes")
+@Getter
+@Setter
+@AllArgsConstructor
+public class Missao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idDaMissao;
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "idDoNinja")
+    private Ninja ninjaAtribuido;
+    @NonNull
+    private String classificacao;
+    @NonNull
+    private String tipoDaMissao;
+    private boolean concluida;
 }
