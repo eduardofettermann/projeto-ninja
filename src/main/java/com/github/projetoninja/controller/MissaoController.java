ee4c0ded45d2d1fa68f30da8fa8fbed6c70bd5e2
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-15 13:23:08 -0300

    feat(controller): adiciona classe MissaoController

---
+++ src/main/java/com/github/projetoninja/controller/MissaoController.java
@@ -0,0 +1,36 @@
+package com.github.projetoninja.controller;
+
+import com.github.projetoninja.model.Missao;
+import com.github.projetoninja.service.MissaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/missoes")
+public class MissaoController {
+    @Autowired
+    private MissaoService missaoService;
+
+    @PostMapping("/cadastrar")
+    public Missao cadastrarMissao(@RequestBody Missao missao) {
+        return missaoService.cadastrarMissao(missao);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Missao> retornarMissaoPorId(@PathVariable Long id){
+        return missaoService.retornarMissaoPeloId(id);
+    }
+
+    @GetMapping
+    public List<Missao> retornarMissoes(){
+        return missaoService.retornarTodasMissoes();
+    }
+
+    @PutMapping("/alterar")
+    public Missao alterarMissao(@RequestParam(value = "id") Long id ,@RequestBody Missao missaoAlterada){
+        return missaoService.alterarMissao(id,missaoAlterada);
+    }
+}
