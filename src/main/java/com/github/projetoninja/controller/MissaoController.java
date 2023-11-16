2a3a209fe5129589603bae3afc30ddd1ee7a20bb
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-15 22:26:42 -0300

    refactor(endpoint/MissaoController): renomea endpoints para nao haver ambiguidade

--- src/main/java/com/github/projetoninja/controller/MissaoController.java
+++ src/main/java/com/github/projetoninja/controller/MissaoController.java
@@ -3,6 +3,7 @@ package com.github.projetoninja.controller;
 import com.github.projetoninja.model.Missao;
 import com.github.projetoninja.service.MissaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,22 +21,22 @@ public class MissaoController {
     }
 
     @GetMapping("/missoes/{id}")
-    public Optional<Missao> retornarMissaoPorId(@PathVariable Long id){
+    public Optional<Missao> retornarMissaoPorId(@PathVariable Long id) {
         return missaoService.retornarMissaoPeloId(id);
     }
 
-    @GetMapping("/missoes")
-    public List<Missao> retornarMissoes(){
-        return missaoService.retornarTodasMissoes();
-    }
-
     @PutMapping("/missoes/alterar")
-    public Missao alterarMissao(@RequestParam(value = "id") Long id ,@RequestBody Missao missaoAlterada){
-        return missaoService.alterarMissao(id,missaoAlterada);
+    public Missao alterarMissao(@RequestParam(value = "id") Long id, @RequestBody Missao missaoAlterada) {
+        return missaoService.alterarMissao(id, missaoAlterada);
     }
 
     @GetMapping("/missoes")
-    public List<Missao> retornarMissaoPorFiltro(@RequestParam(value = "filtrar") String parametro){
-        return missaoService.retornarTodasMissoesPorFiltro(parametro);
+    public List<Missao> retornarMissoes() {
+        return missaoService.retornarMissoes();
+    }
+
+    @GetMapping("/missoes/filtrar")
+    public ResponseEntity<List<Missao>> retornarMissaoPorFiltro(@RequestParam(value = "parametro") String parametro) {
+        return missaoService.retornarMissoesPorFiltro(parametro);
     }
 }
